86adb29b006685079276461820f3db0a3fcdf64f
LucasGonGo <lucasgomes062005@gmail.com>
2025-06-30 15:03:50 -0300

    movement with WASD

--- src/game.py
+++ src/game.py
@@ -11,11 +11,20 @@ class Game:
         self.background = pygame.image.load('assets/images/background_1.png')
 
         self.bg_pos = [self.screen.get_width() // 2, self.screen.get_height() // 2]
-        self.movement = [False, False]
+        self.movement = [False, False, False, False]
 
     def run(self):
         while True:
-            self.bg_pos[1] += (self.movement[1] - self.movement[0])
+            self.screen.fill((14,219,248))
+
+            if self.movement[0]:  # W
+                self.bg_pos[1] -= 5
+            if self.movement[1]:  # S
+                self.bg_pos[1] += 5
+            if self.movement[2]:  # A
+                self.bg_pos[0] -= 5
+            if self.movement[3]:  # D
+                self.bg_pos[0] += 5
 
             self.screen.blit(self.background, self.bg_pos)
 
@@ -25,10 +34,24 @@ class Game:
                     sys.exit()
 
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_w:
                         self.movement[0] = True
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_s:
                         self.movement[1] = True
+                    if event.key == pygame.K_a:
+                        self.movement[2] = True
+                    if event.key == pygame.K_d:
+                        self.movement[3] = True
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_w:
+                        self.movement[0] = False
+                    if event.key == pygame.K_s:
+                        self.movement[1] = False
+                    if event.key == pygame.K_a:
+                        self.movement[2] = False
+                    if event.key == pygame.K_d:
+                        self.movement[3] = False
 
             pygame.display.update()
             self.clock.tick(60)
